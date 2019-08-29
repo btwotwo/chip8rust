a6512bed534833c3fb14909e4654b63aef731c8e
btwotwo <bo20202@yandex.ru>
2019-08-29 16:24:50 +0000

    The big one. Also add devcontainer for the vscode

--- src/opcode/mod.rs
+++ src/opcode/mod.rs
@@ -5,9 +5,9 @@ use std::collections::HashMap;
 macro_rules! opcode_func_map {
     ($($opcode:expr => $func: expr), *) => {
         {
-            let mut map = HashMap::new();
+            let mut map = HashMap::<Opcode, OpcodeImpl>::new();
             $(
-                map.insert($opcode, $func as OpcodeImpl);
+                map.insert($opcode, $func);
             )*
             map
         }
@@ -15,167 +15,212 @@ macro_rules! opcode_func_map {
 }
 
 pub type Opcode = u16;
-type OpcodeImpl = fn(Opcode, &mut Chip) -> ();
+pub type ShouldIncrementPc = bool;
+type OpcodeImpl = fn(&OpcodeHandler, &mut Chip) -> ShouldIncrementPc;
 
 struct OpcodeHandler {
     opcode_map: HashMap<Opcode, OpcodeImpl>,
+    pub current: Opcode,
 }
 
 impl OpcodeHandler {
     pub fn new() -> OpcodeHandler {
         let map = opcode_func_map!(
-            0x00EE => ret,
-            0x1000 => jp,
-            0x2000 => call,
-            0x3000 => se,
-            0x4000 => sne,
-            0x5000 => sre,
-            0x6000 => ld,
-            0x7000 => add,
-            0x8000 => ldr,
-            0x8001 => or,
-            0x8002 => and,
-            0x8003 => xor,
-            0x8004 => addreg,
-            0x8005 => subreg,
-            0x8006 => shiftr
+            0x00EE => OpcodeHandler::ret,
+            0x1000 => OpcodeHandler::jp,
+            0x2000 => OpcodeHandler::call,
+            0x3000 => OpcodeHandler::se,
+            0x4000 => OpcodeHandler::sne,
+            0x5000 => OpcodeHandler::sre,
+            0x6000 => OpcodeHandler::ld,
+            0x7000 => OpcodeHandler::add,
+            0x8000 => OpcodeHandler::ldr,
+            0x8001 => OpcodeHandler::or,
+            0x8002 => OpcodeHandler::and,
+            0x8003 => OpcodeHandler::xor,
+            0x8004 => OpcodeHandler::addreg,
+            0x8005 => OpcodeHandler::subreg,
+            0x8006 => OpcodeHandler::shiftr,
+            0x8007 => OpcodeHandler::sub,
+            0x800E => OpcodeHandler::shiftl,
+            0x9000 => OpcodeHandler::snexy
         );
 
-        OpcodeHandler { opcode_map: map }
+        OpcodeHandler {
+            opcode_map: map,
+            current: 0x00E0,
+        }
     }
-}
 
-///Return from a subroutine (00EE)
-fn ret(_: Opcode, chip: &mut Chip) {
-    chip.program_counter
-        .set(chip.stack[chip.stack_pointer as usize]);
-    chip.stack_pointer -= 1;
-}
+    pub fn next(&self, opcode: Opcode, chip: &mut Chip) {
+        let should_increment = match self.opcode_map.get(&opcode) {
+            Some(func) => func(self, chip),
+            None => {
+                println!("Opcode not found! Skipping...");
+                true
+            }
+        };
 
-///Jump to address NNN (1NNN)
-fn jp(opcode: Opcode, chip: &mut Chip) {
-    chip.program_counter.set(opcode & 0x0FFF);
-}
-
-///Call subroutine at NNN (2NNN)
-fn call(opcode: Opcode, chip: &mut Chip) {
-    chip.stack_pointer += 1;
-    chip.stack[chip.stack_pointer as usize] = chip.program_counter.get();
-    chip.program_counter.set(opcode & 0x0FFF);
-}
-
-//Skip if Vx equals NN (3XNN)
-fn se(opcode: Opcode, chip: &mut Chip) {
-    let register_index = (opcode & 0x0f00) >> 8;
-    let compare = (opcode & 0x00FF) as u8;
-    let register = chip.v[(opcode, Position::X)];
-
-    if register == compare {
-        chip.program_counter.skip(1);
-    } else {
-        chip.program_counter.increment();
+        if should_increment {
+            chip.program_counter.increment();
+        }
     }
-}
 
-//Skip if Vx doesn't equal NN (4XNN)
-fn sne(opcode: Opcode, chip: &mut Chip) {
-    let register = chip.v[(opcode, Position::X)];
-    let to_compare = (opcode & 0x00FF) as u8;
-
-    if register != to_compare {
-        chip.program_counter.skip(1);
-    } else {
-        chip.program_counter.increment();
+    ///Return from a subroutine (00EE)
+    fn ret(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.program_counter
+            .set(chip.stack[chip.stack_pointer as usize]);
+        chip.stack_pointer -= 1;
+        false
     }
-}
 
-///Skip if Vx equal Vy (5XY0)
-fn sre(opcode: Opcode, chip: &mut Chip) {
-    let first_register = chip.v[(opcode, Position::X)];
-    let second_register = chip.v[(opcode, Position::Y)];
-
-    if first_register == second_register {
-        chip.program_counter.skip(1);
-    } else {
-        chip.program_counter.increment();
+    ///Jump to address NNN (1NNN)
+    fn jp(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.program_counter.set(self.current & 0x0FFF);
+        false
     }
-}
 
-///Set Vx to NN (6XNN)
-fn ld(opcode: Opcode, chip: &mut Chip) {
-    chip.v[(opcode, Position::X)] = (opcode & 0x00FF) as u8;
-    chip.program_counter.increment();
-}
+    ///Call subroutine at NNN (2NNN)
+    fn call(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.stack_pointer += 1;
+        chip.stack[chip.stack_pointer as usize] = chip.program_counter.get();
+        chip.program_counter.set(self.current & 0x0FFF);
+        true
+    }
 
-///Add NN to Vx, carry flag not changed (7XNN)
-fn add(opcode: Opcode, chip: &mut Chip) {
-    let index = Registers::get_index(opcode, Position::X);
-    chip.v.add_immediate(index, (opcode & 0x00FF) as u8);
-    chip.program_counter.increment();
-}
+    //Skip if Vx equals NN (3XNN)
+    fn se(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let register_index = (self.current & 0x0f00) >> 8;
+        let compare = (self.current & 0x00FF) as u8;
+        let register = chip.v[(self.current, Position::X)];
 
-///Set Vx to the value of Vy (8XY0)
-fn ldr(opcode: Opcode, chip: &mut Chip) {
-    chip.v[(opcode, Position::X)] = chip.v[(opcode, Position::Y)];
-    chip.program_counter.increment();
-}
+        if register == compare {
+            chip.program_counter.skip(1);
+            false
+        } else {
+            true
+        }
+    }
 
-///Set Vx to the result of bitwise OR with Vy (8XY1)
-fn or(opcode: Opcode, chip: &mut Chip) {
-    chip.v[(opcode, Position::X)] |= chip.v[(opcode, Position::Y)];
-    chip.program_counter.increment();
-}
+    //Skip if Vx doesn't equal NN (4XNN)
+    fn sne(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let register = chip.v[(self.current, Position::X)];
+        let to_compare = (self.current & 0x00FF) as u8;
 
-///Set Vx to the result of bitwise AND with Vy (8XY2)
-fn and(opcode: Opcode, chip: &mut Chip) {
-    chip.v[(opcode, Position::X)] &= chip.v[(opcode, Position::Y)];
-    chip.program_counter.increment();
-}
+        if register != to_compare {
+            chip.program_counter.skip(1);
+            false
+        } else {
+            true
+        }
+    }
 
-///Set Vx to the result of bitwise XOR with Vy(8XY3)
-fn xor(opcode: Opcode, chip: &mut Chip) {
-    chip.v[(opcode, Position::X)] ^= chip.v[(opcode, Position::Y)];
-    chip.program_counter.increment();
-}
+    ///Skip if Vx equal Vy (5XY0)
+    fn sre(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let first_register = chip.v[(self.current, Position::X)];
+        let second_register = chip.v[(self.current, Position::Y)];
 
-///Add Vy to VX, change carry flag if there's a borrow (8XY4)
-fn addreg(opcode: Opcode, chip: &mut Chip) {
-    let left = chip.v[(opcode, Position::X)];
-    let right = chip.v[(opcode, Position::Y)];
+        if first_register == second_register {
+            chip.program_counter.skip(1);
+            false
+        } else {
+            true
+        }
+    }
 
-    let (result, carried) = left.overflowing_add(right);
+    ///Set Vx to NN (6XNN)
+    fn ld(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[(self.current, Position::X)] = (self.current & 0x00FF) as u8;
+        true
+    }
 
-    chip.v[(opcode, Position::X)] = result;
-    chip.v.set_carry(carried);
-    chip.program_counter.increment();
-}
+    ///Add NN to Vx, carry flag not changed (7XNN)
+    fn add(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let index = Registers::get_index(self.current, Position::X);
+        chip.v.add_immediate(index, (self.current & 0x00FF) as u8);
+        true
+    }
 
-///Subtract Vy from Vx, change carry flag if there's a borrow (8XY5)
-fn subreg(opcode: Opcode, chip: &mut Chip) {
-    let (result, carried) =
-        chip.v[(opcode, Position::X)].overflowing_sub(chip.v[(opcode, Position::Y)]);
+    ///Set Vx to the value of Vy (8XY0)
+    fn ldr(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[(self.current, Position::X)] = chip.v[(self.current, Position::Y)];
+        true
+    }
 
-    chip.v[(opcode, Position::X)] = result;
-    chip.v.set_carry(carried);
-    chip.program_counter.increment();
-}
+    ///Set Vx to the result of bitwise OR with Vy (8XY1)
+    fn or(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[(self.current, Position::X)] |= chip.v[(self.current, Position::Y)];
+        true
+    }
 
-///Store least significant bit of Vx in VF and then shift Vx to the right by 1
-fn shiftr(opcode: Opcode, chip: &mut Chip) {
-    chip.v[0xF] = chip.v[(opcode, Position::X)] & 1;
-    chip.v[(opcode, Position::X)] >>= 1;
-    chip.program_counter.increment();
-}
+    ///Set Vx to the result of bitwise AND with Vy (8XY2)
+    fn and(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[(self.current, Position::X)] &= chip.v[(self.current, Position::Y)];
+        true
+    }
 
-fn sub(opcode: Opcode, chip: &mut Chip) {
-    let x = chip.v[(opcode, Position::X)];
-    let y = chip.v[(opcode, Position::Y)];
-    let (result, carried) = y.overflowing_sub(x);
+    ///Set Vx to the result of bitwise XOR with Vy(8XY3)
+    fn xor(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[(self.current, Position::X)] ^= chip.v[(self.current, Position::Y)];
+        true
+    }
 
-    chip.v[(opcode, Position::X)] = result;
+    ///Add Vy to VX, change carry flag if there's a borrow (8XY4)
+    fn addreg(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let left = chip.v[(self.current, Position::X)];
+        let right = chip.v[(self.current, Position::Y)];
 
-    chip.v.set_carry(carried);
-    chip.program_counter.increment();
+        let (result, carried) = left.overflowing_add(right);
+
+        chip.v[(self.current, Position::X)] = result;
+        chip.v.set_carry(carried);
+        true
+    }
+
+    ///Subtract Vy from Vx, change carry flag if there's a borrow (8XY5)
+    fn subreg(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let (result, carried) = chip.v[(self.current, Position::X)]
+            .overflowing_sub(chip.v[(self.current, Position::Y)]);
+
+        chip.v[(self.current, Position::X)] = result;
+        chip.v.set_carry(carried);
+        true
+    }
+
+    ///Store least significant bit of Vx in VF and then shift Vx to the right by 1 (8X06)
+    fn shiftr(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        chip.v[0xF] = chip.v[(self.current, Position::X)] & 1;
+        chip.v[(self.current, Position::X)] >>= 1;
+        true
+    }
+
+    ///Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't. (8X07)
+    fn sub(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let x = chip.v[(self.current, Position::X)];
+        let y = chip.v[(self.current, Position::Y)];
+        let (result, carried) = y.overflowing_sub(x);
+
+        chip.v[(self.current, Position::X)] = result;
+
+        chip.v.set_carry(carried);
+        true
+    }
+
+    ///Stores the most significant bit of VX in VF and then shifts VX to the left by 1 (8X0E)
+    fn shiftl(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        let index = Registers::get_index(self.current, Position::X);
+        chip.v[0xF] = (chip.v[index] >= 128) as u8;
+        chip.v[index] <<= 1;
+        true
+    }
+
+    fn snexy(&self, chip: &mut Chip) -> ShouldIncrementPc {
+        if chip.v[(self.current, Position::X)] != chip.v[(self.current, Position::Y)] {
+            chip.program_counter.skip(1);
+        }
+
+        true
+    }
 }
 
 #[cfg(test)]
--- src/opcode/test.rs
+++ src/opcode/test.rs
@@ -1,16 +1,22 @@
 use super::*;
 
-fn assert_pc_increment(chip: &Chip) {
-    assert_eq!(chip.program_counter, 514, "Forgot to increment program counter.");
+
+fn prepare(opcode: Opcode) -> (Chip, OpcodeHandler) {
+    let mut handler = OpcodeHandler::new();
+
+    handler.current = opcode;
+
+    (Chip::new(), handler)
 }
 
 #[test]
 fn ret_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x00EE);
+
     chip.stack_pointer = 10;
     chip.stack[chip.stack_pointer as usize] = 0x1234;
 
-    ret(0x00EE, &mut chip);
+    handler.ret(&mut chip);
 
     assert_eq!(chip.program_counter, 0x1234);
     assert_eq!(chip.stack_pointer, 9);
@@ -18,22 +24,22 @@ fn ret_test() {
 
 #[test]
 fn jp_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x1DEA);
     chip.program_counter.set(0xABC);
 
-    jp(0x1DEA, &mut chip);
+    handler.jp(&mut chip);
 
     assert_eq!(chip.program_counter, 0xDEA)
 }
 
 #[test]
 fn call_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x2DEA);
     chip.program_counter.set(12);
 
     let opcode = 0x2DEA;
 
-    call(0x2DEA, &mut chip);
+    handler.call(&mut chip);
 
     assert_eq!(chip.program_counter, 0xDEA);
     assert_eq!(chip.stack_pointer, 1);
@@ -42,39 +48,43 @@ fn call_test() {
 
 #[test]
 fn se_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x3412);
     chip.program_counter.set(12);
     chip.v[4] = 0x12;
     chip.v[5] = 0x10;
 
-    se(0x3412, &mut chip);
+    handler.se(&mut chip);
 
     assert_eq!(chip.program_counter, 16);
 
-    se(0x3512, &mut chip);
+    handler.current = 0x3512;
+
+    handler.se(&mut chip);
 
     assert_eq!(chip.program_counter, 18);
 }
 
 #[test]
 fn sne_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x4412);
     chip.program_counter.set(12);
     chip.v[4] = 0x12;
     chip.v[5] = 0x10;
 
-    sne(0x4412, &mut chip);
+    handler.sne(&mut chip);
 
     assert_eq!(chip.program_counter, 14);
 
-    sne(0x4512, &mut chip);
+    handler.current = 0x4512;
+
+    handler.sne(&mut chip);
 
     assert_eq!(chip.program_counter, 18);
 }
 
 #[test]
 fn sre_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x5450);
 
     chip.program_counter.set(12);
 
@@ -82,192 +92,193 @@ fn sre_test() {
     chip.v[5] = 0x12;
     chip.v[6] = 0x13;
 
-    sre(0x5450, &mut chip);
+    handler.sre(&mut chip);
 
     assert_eq!(chip.program_counter, 16);
 
-    sre(0x5460, &mut chip);
+    handler.current = 0x5460;
+
+    handler.sre(&mut chip);
 
     assert_eq!(chip.program_counter, 18)
 }
 
 #[test]
 fn ld_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x6EAB);
 
     chip.v[0xE] = 0xAB;
 
-    ld(0x6EAB, &mut chip);
+    handler.ld(&mut chip);
 
     assert_eq!(chip.v[0xE], 0xAB);
-    assert_pc_increment(&mut chip);
+    
 }
 
 #[test]
 fn add_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x75AB);
 
     chip.v[0x5] = 0x1;
 
-    add(0x75AB, &mut chip);
+    handler.add(&mut chip);
 
     assert_eq!(chip.v[0x5], 0xAC);
-    assert_pc_increment(&mut chip);
+    
 }
 
 #[test]
 fn add_overflow_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x7502);
 
     chip.v[0x5] = 0xFF;
 
-    add(0x7502, &mut chip);
+    handler.add(&mut chip);
 
     assert_eq!(chip.v[0x5], 0x1);
-    assert_pc_increment(&mut chip);
+    
 }
 
 #[test]
 fn ldr_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8120);
     chip.v[1] = 111;
     chip.v[2] = 222;
 
-    ldr(0x8120, &mut chip);
+    handler.ldr(&mut chip);
 
     assert_eq!(chip.v[1], 222);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn or_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8121);
     chip.v[1] = 0x12;
     chip.v[2] = 0x34;
 
-    or(0x8121, &mut chip);
+    handler.or(&mut chip);
     assert_eq!(chip.v[1], 0x36);
     assert_eq!(chip.v[2], 0x34);
 
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn and_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8122);
 
     chip.v[1] = 0x12;
     chip.v[2] = 0x34;
 
-    and(0x8122, &mut chip);
+    handler.and(&mut chip);
 
     assert_eq!(chip.v[1], 0x10);
     assert_eq!(chip.v[2], 0x34);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn xor_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8123);
 
     chip.v[1] = 0x12;
     chip.v[2] = 0x34;
 
-    xor(0x8123, &mut chip);
+    handler.xor(&mut chip);
 
     assert_eq!(chip.v[1], 0x26);
     assert_eq!(chip.v[2], 0x34);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn addreg_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8014);
 
     chip.v[0] = 0x20;
     chip.v[1] = 0x01;
     chip.v[0xF] = 1;
 
-    addreg(0x8014, &mut chip);
+    handler.addreg(&mut chip);
 
     assert_eq!(chip.v[0], 0x21);
     assert_eq!(chip.v[1], 0x01);
     assert_eq!(chip.v[0xF], 0);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn addreg_carry_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8014);
 
     chip.v[0] = 0xFF;
     chip.v[1] = 0x02;
     chip.v[0xF] = 0;
 
-    addreg(0x8014, &mut chip);
+    handler.addreg(&mut chip);
 
     assert_eq!(chip.v[0], 0x01);
     assert_eq!(chip.v[1], 0x02);
     assert_eq!(chip.v[0xF], 1);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn subreg_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8015);
 
     chip.v[0] = 0x20;
     chip.v[1] = 0x01;
     chip.v[0xF] = 1;
 
-    subreg(0x8015, &mut chip);
+    handler.subreg(&mut chip);
 
     assert_eq!(chip.v[0], 0x1F);
     assert_eq!(chip.v[1], 0x01);
     assert_eq!(chip.v[0xF], 0);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn subreg_carry_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8015);
 
     chip.v[0] = 0x00;
     chip.v[1] = 0x01;
     chip.v[0xF] = 0;
 
-    subreg(0x8015, &mut chip);
+    handler.subreg(&mut chip);
 
     assert_eq!(chip.v[0], 0xFF);
     assert_eq!(chip.v[1], 0x01);
     assert_eq!(chip.v[0xF], 1);
-    assert_pc_increment(&chip);
+    
 }
 
 #[test]
 fn shiftr_test() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8016);
 
     chip.v[0] = 0x10;
     chip.v[0xF] = 1;
 
-    shiftr(0x8016, &mut chip);
+    handler.shiftr(&mut chip);
 
     assert_eq!(chip.v[0], 0x8);
     assert_eq!(chip.v[0xF], 0);
-    assert_pc_increment(&chip);
+    
 }
-
 
 #[test]
 fn sub_test() {
-    let mut chip = Chip::new();
-    
+    let (mut chip, mut handler) = prepare(0x8107);
+
     chip.v[0] = 0;
     chip.v[1] = 1;
     chip.v[0xF] = 1;
 
-    sub(0x8107, &mut chip);
+    handler.sub(&mut chip);
 
     assert_eq!(chip.v[1], 255);
     assert_eq!(chip.v[0xF], 1);
@@ -275,14 +286,26 @@ fn sub_test() {
 
 #[test]
 fn sub_test_overflow() {
-    let mut chip = Chip::new();
+    let (mut chip, mut handler) = prepare(0x8017);
 
     chip.v[0] = 0;
     chip.v[1] = 1;
     chip.v[0xF] = 1;
 
-    sub(0x8017, &mut chip);   
+    handler.sub(&mut chip);
 
     assert_eq!(chip.v[0], 1);
     assert_eq!(chip.v[0xF], 0);
+}
+
+#[test]
+fn shiftl_test_significant_one() {
+    let (mut chip, mut handler) = prepare(0x801E);
+    chip.v[0] = 0b100_0100_1;
+    chip.v[0xf] = 0;
+
+    handler.shiftl(&mut chip);
+
+    assert_eq!(chip.v[0], 0b0001_001_0);
+    assert_eq!(chip.v[0xf], 1);
 }
